6714776539c2d2a0914626c7a85f02e586ff5c39
Othman AMD <red.fantastic@hotmail.com>
2017-09-19 19:58:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(index)
-  if index.between(0,8)
-    position_taken?
-  else
-    return "Out of Range"
+  if position_taken?(board,index_number) == false && index.between(0,8)
+    return true 
   end
 end
 
