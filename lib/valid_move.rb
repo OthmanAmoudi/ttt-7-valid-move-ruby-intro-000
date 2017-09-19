fd96ca06c610ec04096b642f0a4d3a429c562100
Othman AMD <red.fantastic@hotmail.com>
2017-09-19 19:35:05 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
 # code your #valid_move? method here
+def valid_move?(user_input)
+  if user_input.between(0,8)
+    position_taken?
+  else
+    return "Out of Range"
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index_number)
+  position = board[index_number]
+  if position == " " || position == "" || position == nil
+    return false
+  else
+    return true
+  end
+end
